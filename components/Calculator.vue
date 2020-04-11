7cbf3d6cdd1a6c9d453d1a9565872539dee280df
Brent Miller <brentwmiller93@gmail.com>
2020-04-10 22:47:40 -0400

    sets up initial project; builds ratio calculation

---
+++ components/Calculator.vue
@@ -0,0 +1,101 @@
+<template>
+  <form class="grid grid-cols-2 gap-4">
+    <div>
+      <div class="flex flex-col mb-4">
+        <label for="x1">Width 1</label>
+        <input
+          class="border"
+          v-model="x1"
+          v-on:input="solve(undefined, y2, x1, y1)"
+          id="x1"
+          type="text"
+        />
+      </div>
+
+      <div class="flex flex-col">
+        <label for="y1">Height 1</label>
+        <input
+          class="border"
+          v-model="y1"
+          v-on:input="solve(x2, undefined, x1, y1)"
+          id="y1"
+          type="text"
+        />
+      </div>
+    </div>
+
+    <div>
+      <div class="flex flex-col mb-4">
+        <label for="x2">Width 2</label>
+        <input
+          class="border"
+          v-model="x2"
+          v-on:input="solve(x2, undefined, x1, y1)"
+          id="x2"
+          type="text"
+        />
+      </div>
+
+      <div class="flex flex-col">
+        <label for="y2">Height 2</label>
+        <input
+          class="border"
+          v-model="y2"
+          v-on:input="solve(undefined, y2, x1, y1)"
+          id="y2"
+          type="text"
+        />
+      </div>
+    </div>
+
+    <pre>
+      x1: {{x1}}
+      y1: {{y1}}
+      x2: {{x2}}
+      y2: {{y2}}
+      ar: {{ratio.x}}:{{ratio.y}}
+    </pre>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      x1: 1920,
+      y1: 1080,
+      x2: 0,
+      y2: 0,
+      ratio: {
+        x: 16,
+        y: 9
+      }
+    };
+  },
+  methods: {
+    /**
+     * Solve for the 4th value
+     * @param Number width       Numerator from the right side of the equation
+     * @param Number height      Denominator from the right side of the equation
+     * @param Number numerator   Numerator from the left side of the equation
+     * @param Number denominator Denominator from the left side of the equation
+     * @return Number
+     */
+    solve: function(width, height, numerator, denominator) {
+      // solve for width
+      if (undefined !== width) {
+        this.y2 = Math.round(width / (numerator / denominator));
+      }
+      // solve for height
+      else if (undefined !== height) {
+        this.x2 = Math.round(height * (numerator / denominator));
+      } else {
+        console.error("failed");
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "~/components/Calculator.vue";
+
+export default {
+  components: {
+    Calculator
+  }
+};
+</script>
+
+<style>
+</style>
